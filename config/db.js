4091a57b9ae95dc1adecbecb2c16374f9575730a
Sumit Bhimte <43449951+sumitbhimte@users.noreply.github.com>
2023-11-20 22:38:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import colors from "colors";
+const MONGO_URL = "URL_TO_DATABASE"
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect(process.MONGO_URL);
         console.log(
             `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
         );
     } catch (error) {
-        console.log(`Errro in Mongodb ${error}`.bgRed.white);
+        console.log(`Errror in Mongodb ${error}`.bgRed.white);
     }
 };
 
